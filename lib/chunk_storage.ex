0cf4de46d2f5577d68a9c6cb40b24cc08b83f024
Gjum <code.gjum@gmail.com>
2017-05-10 00:30:38 +0200

    use Map.get_and_update instead of fetch+put

--- lib/chunk_storage.ex
+++ lib/chunk_storage.ex
@@ -43,15 +43,17 @@ defmodule ChunkFix.ChunkStorage do
   end
 
   def handle_cast({:store, position, packet_data}, storage) do
-    case Map.fetch(storage, position) do
-      :error ->
-        Logger.info "Storing new chunk at #{inspect ChunkFix.Protocol.nice_pos position}, size: #{inspect byte_size packet_data}"
-      {:ok, old_packet} ->
-        if byte_size(packet_data) != byte_size(old_packet) do
-          Logger.info "Replacing chunk at #{inspect ChunkFix.Protocol.nice_pos position}, new size: #{inspect byte_size packet_data}, was: #{inspect byte_size old_packet}"
-        end
+    {old_packet, storage} = Map.get_and_update(storage, position, packet_data)
+    if old_packet == nil do
+      Logger.info "Storing new chunk at #{inspect ChunkFix.Protocol.nice_pos position}, size: #{inspect byte_size packet_data}"
+    else
+      if byte_size(packet_data) != byte_size(old_packet) do
+        Logger.info "Replacing chunk at #{inspect ChunkFix.Protocol.nice_pos position}, new size: #{inspect byte_size packet_data}, was: #{inspect byte_size old_packet}"
+      else
+        Logger.info "Replacing chunk at #{inspect ChunkFix.Protocol.nice_pos position}, same size: #{inspect byte_size packet_data}"
+      end
     end
-    {:noreply, Map.put(storage, position, packet_data)}
+    {:noreply, storage}
   end
 
   def handle_call({:retrieve, position}, _from, storage) do
