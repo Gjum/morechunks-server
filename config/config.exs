52c2277d04e2f40bf3e23c99580cde94d4b6d520
Gjum <code.gjum@gmail.com>
2017-11-09 20:16:46 +0100

    add logger_file_backend

--- config/config.exs
+++ config/config.exs
@@ -1,31 +1,14 @@
 use Mix.Config
 
-listen_port_from_env =
-  with port_str <- System.get_env("PORT"),
-       {port, ""} <- :string.to_integer(port_str) do
-    port
-  else
-    _ -> 12312
-  end
-
 config :morechunks,
   max_chunks_per_second: 80,
-  listen_ip: {127, 0, 0, 1},
-  listen_port: listen_port_from_env
+  listen_ip: {0, 0, 0, 0},
+  listen_port: 12312
 
-# You can configure for your application as:
-#
-#     config :morechunks, key: :value
-#
-# Or configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
+config :logger,
+  backends: [
+    :console,
+    {LoggerFileBackend, :file_log}
+  ]
 
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+config :logger, :file_log, path: "morechunks.log"
--- mix.exs
+++ mix.exs
@@ -2,23 +2,26 @@ defmodule MoreChunks.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :morechunks,
-      version: "0.1.0",
+    [
+      app: :morechunks,
+      version: "1.0.0",
       elixir: "~> 1.4",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
   def application do
-    [ extra_applications: [:logger],
-      mod: {MoreChunks, []},
+    [
+      extra_applications: [:logger],
+      mod: {MoreChunks, []}
     ]
   end
 
   defp deps do
     [
+      {:logger_file_backend, "0.0.10"}
     ]
   end
 end
